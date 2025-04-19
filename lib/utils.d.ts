c13adb464d24bf0ef8f3daf3334352e977dc2e2e
GitHub <rakshitbharat@gmail.com>
2025-04-19 13:50:34 +0530

    -

--- lib/utils.d.ts
+++
@@ -1,9 +0,0 @@
-import { LoggerConfig, LogLevel } from './types.js';
-import { COLORS } from './constants.js';
-type ColorMap = {
-    [K in keyof typeof COLORS]: string;
-};
-export declare const formatJSON: (obj: unknown, colors?: ColorMap) => string;
-export declare const formatMessage: (type: LogLevel, messageParts: unknown[], config: Required<LoggerConfig>) => string;
-export declare const getDateString: (date?: Date) => string;
-export {};
